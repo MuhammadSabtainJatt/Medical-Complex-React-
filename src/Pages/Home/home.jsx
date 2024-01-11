c58f29861e9298c0455ba1f10949f0a50c0c6aad
Muhammad Sabtain <subtainjutt26@gmail.com>
2024-01-11 21:52:19 +0500

    Tweaks in Home page

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,12 +1,13 @@
-import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
+import { faCalendarDays, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 import React, { useState } from 'react';
 import { Card } from 'react-bootstrap';
 import { firestore } from '../../Config/config';
 import { message } from 'antd';
+import departmentImage from '../../Asset/Image/departments-991x661.jpg'
 
-const InitialState={
+const InitialState = {
   departments: '',
   phone: '',
   email: '',
@@ -24,10 +25,10 @@ export default function Home() {
   };
 
   const handleSubmit = async () => {
-    const { departments,phone,email } = formData
+    const { departments, phone, email } = formData
 
     const today = new Date()
-    if (!phone || !departments || !email ) {
+    if (!phone || !departments || !email) {
       return message.error("Please Enter all input feilds Correct")
     }
     const appointmentsData = {
@@ -42,7 +43,7 @@ export default function Home() {
       console.error("Error adding document: ", e);
       message.error("Something went wrong ")
     }
-setFormData(InitialState)
+    setFormData(InitialState)
   }
 
 
@@ -160,61 +161,124 @@ setFormData(InitialState)
               </div>
             </Card.Header>
             <Card.Body>
-             <div className="row">
-        <div className="col-12 col-md-6 p-2">
-          <select
-            id="selectOption"
-            className='input shadow'
-            name='departments'
-            value={formData.departments}
-            onChange={handleChange}
-          >
-            <option value="">Select Departments</option>
-            <option value="Neurology">Neurology</option>
-            <option value="Dentistry">Dentistry</option>
-            <option value="Cardiology">Cardiology</option>
-            <option value="Pediatrics">Pediatrics</option>
-            <option value="Pulmonology">Pulmonology</option>
-            <option value="Ophthalmology">Ophthalmology</option>
-            <option value="Diagnostics">Diagnostics</option>
-          </select>
-        </div>
-        <div className="col-12 col-md-6 p-2">
-          <input
-            type="text"
-            className='input shadow'
-            placeholder='Phone'
-            name='phone'
-            value={formData.phone}
-            onChange={handleChange}
-          />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-12 col-md-6 p-2">
-          <input
-            type="email"
-            className='input shadow'
-            placeholder='Email'
-            name='email'
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="col-12 col-md-6 p-2 text-center">
-          <div
-            className="btn input w-100 shadow"
-            style={{ background: 'linear-gradient(to right, #000, gray)', border: "none" }}
-            onClick={handleSubmit}
-          >
-            <p className=' my-2 text-white'>Add Appointment</p>
-          </div>
-        </div>
-      </div>
+              <div className="row">
+                <div className="col-12 col-md-6 p-2">
+                  <select
+                    id="selectOption"
+                    className='input shadow'
+                    name='departments'
+                    value={formData.departments}
+                    onChange={handleChange}
+                  >
+                    <option value="">Select Departments</option>
+                    <option value="Neurology">Neurology</option>
+                    <option value="Dentistry">Dentistry</option>
+                    <option value="Cardiology">Cardiology</option>
+                    <option value="Pediatrics">Pediatrics</option>
+                    <option value="Pulmonology">Pulmonology</option>
+                    <option value="Ophthalmology">Ophthalmology</option>
+                    <option value="Diagnostics">Diagnostics</option>
+                  </select>
+                </div>
+                <div className="col-12 col-md-6 p-2">
+                  <input
+                    type="text"
+                    className='input shadow'
+                    placeholder='Phone'
+                    name='phone'
+                    value={formData.phone}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-12 col-md-6 p-2">
+                  <input
+                    type="email"
+                    className='input shadow'
+                    placeholder='Email'
+                    name='email'
+                    value={formData.email}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="col-12 col-md-6 p-2 text-center">
+                  <div
+                    className="btn input w-100 shadow"
+                    style={{ background: 'linear-gradient(to right, #000, gray)', border: "none" }}
+                    onClick={handleSubmit}
+                  >
+                    <p className=' my-2 text-white'>Add Appointment</p>
+                  </div>
+                </div>
+              </div>
             </Card.Body>
           </Card>
         </div>
+        <hr />
       </div>
+      <div className="row">
+        <div className="col-12 col-lg-5 text-center p-2">
+          <div style={{ backgroundColor: "lightgray" }} className='p-5'>
+            <h2>
+              About Clinic
+            </h2>
+            <p className='f-smaller'>
+              Our clinic specializes in a wide variety of health issues, related to absolutely any age or severity level. Our seasoned team of highly trained physicians and practical nurses will be glad to help you.
+            </p>
+            <div className="row">
+              <div className="col-12 col-sm-6 my-2">
+                <h5 className='d-inline me-2'><FontAwesomeIcon icon={faCheck} className='text-warning' /></h5>
+                <h5 className='d-inline' >Qualified Doctors</h5>
+              </div>
+              <div className="col-12 col-sm-6 my-2">
+                <h5 className='d-inline me-2'><FontAwesomeIcon icon={faCheck} className='text-warning' /></h5>
+                <h5 className='d-inline' >Emergency Help</h5>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12 col-sm-6 my-2">
+                <h5 className='d-inline me-2'><FontAwesomeIcon icon={faCheck} className='text-warning' /></h5>
+                <h5 className='d-inline' >Individual Approach</h5>
+              </div>
+              <div className="col-12 col-sm-6 my-2">
+                <h5 className='d-inline me-2'><FontAwesomeIcon icon={faCheck} className='text-warning' /></h5>
+                <h5 className='d-inline' >Modern Equipment</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-lg-7 p-2">
+          <div style={{ backgroundColor: "lightgray" }} className='p-2 text-center'>
+
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="row">
+        <div className="col-12 col-lg-5">
+          <img src={departmentImage} className='w-100' alt="" />
+        </div>
+        <div className="col-12 col-lg-4" style={{ display: "flex", flexDirection: 'column', justifyContent: 'center', alignItems: "left", backgroundColor: "lightgray" }}>
+          <h3>
+            Departments
+          </h3>
+          <p className='my-3 f-smaller'>
+            Click the department name you are looking for to view more information and access resources specific to this area.
+          </p>
+          <button
+            className="custom-button"
+          >
+            More Departments
+          </button>
+        </div>
+        <div className="col-12 col-lg-3">khgcqudwa</div>
+      </div>
+
+
+
+
+
     </div>
   );
 }
