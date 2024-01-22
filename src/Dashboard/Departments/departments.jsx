14df6da1fef0af68842037b988d63a27848fc222
Muhammad Sabtain <subtainjutt26@gmail.com>
2024-01-22 21:14:54 +0500

    Tweaks in Departments File

--- src/Dashboard/Departments/departments.jsx
+++ src/Dashboard/Departments/departments.jsx
@@ -4,17 +4,29 @@ export default function departments() {
     <div className="container-fluid">
       <div className="container">
         <div className="row">
-          <div className="col-12 col-md-6 col-lg-3 p-3">
-jhasgdjgakj
+          <div className="col-12 col-md-6 col-lg-3 p-3 d-flex justify-content-center text-center flex-column ">
           </div>
-          <div className="col-12 col-md-6 col-lg-3 p-3">
-skdufchsiduf
+          <div className="col-12 col-md-6 col-lg-3 p-3 d-flex justify-content-center text-center flex-column">
+            skdufchsiduf
           </div>
-          <div className="col-12 col-md-6 col-lg-3 p-3">
-dskhfcslefi
+          <div className="col-12 col-md-6 col-lg-3 p-3 d-flex justify-content-center text-center flex-column">
+            dskhfcslefi
           </div>
-          <div className="col-12 col-md-6 col-lg-3 p-3">
-dskhfcslefi
+          <div className="col-12 col-md-6 col-lg-3 p-3 d-flex justify-content-center text-center flex-column">
+            dskhfcslefi
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 col-md-6 col-lg-3 p-3 d-flex justify-content-center text-center flex-column ">
+          </div>
+          <div className="col-12 col-md-6 col-lg-3 p-3 d-flex justify-content-center text-center flex-column">
+            skdufchsiduf
+          </div>
+          <div className="col-12 col-md-6 col-lg-3 p-3 d-flex justify-content-center text-center flex-column">
+            dskhfcslefi
+          </div>
+          <div className="col-12 col-md-6 col-lg-3 p-3 d-flex justify-content-center text-center flex-column">
+            dskhfcslefi
           </div>
         </div>
       </div>
