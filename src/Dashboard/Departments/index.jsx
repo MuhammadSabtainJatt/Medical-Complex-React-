8d31aa660d67db53f2b351e950cfcf47ebe93bf3
Muhammad Sabtain <subtainjutt26@gmail.com>
2024-02-02 21:51:21 +0500

    Tweaks in Code

--- src/Dashboard/Departments/index.jsx
+++ src/Dashboard/Departments/index.jsx
@@ -5,3 +5,94 @@ export default function index() {
     <Departments />
   )
 }
+import React from 'react'
+import Departments from './departmentDetail'
+export default function index() {
+  return (
+    <Departments />
+  )
+}
+import React from 'react'
+import Departments from './departmentDetail'
+export default function index() {
+  return (
+    <Departments />
+  )
+}
+import React from 'react'
+import Departments from './departmentDetail'
+export default function index() {
+  return (
+    <Departments />
+  )
+}
+import React from 'react'
+import Departments from './departmentDetail'
+export default function index() {
+  return (
+    <Departments />
+  )
+}
+import React from 'react'
+import Departments from './departmentDetail'
+export default function index() {
+  return (
+    <Departments />
+  )
+}
+import React from 'react'
+import Departments from './departmentDetail'
+export default function index() {
+  return (
+    <Departments />
+  )
+}
+import React from 'react'
+import Departments from './departmentDetail'
+export default function index() {
+  return (
+    <Departments />
+  )
+}
+import React from 'react'
+import Departments from './departmentDetail'
+export default function index() {
+  return (
+    <Departments />
+  )
+}
+import React from 'react'
+import Departments from './departmentDetail'
+export default function index() {
+  return (
+    <Departments />
+  )
+}
+import React from 'react'
+import Departments from './departmentDetail'
+export default function index() {
+  return (
+    <Departments />
+  )
+}
+import React from 'react'
+import Departments from './departmentDetail'
+export default function index() {
+  return (
+    <Departments />
+  )
+}
+import React from 'react'
+import Departments from './departmentDetail'
+export default function index() {
+  return (
+    <Departments />
+  )
+}
+import React from 'react'
+import Departments from './departmentDetail'
+export default function index() {
+  return (
+    <Departments />
+  )
+}
